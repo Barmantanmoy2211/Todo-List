ff0a2901346df8d4223ba7a327288b02b57bff1b
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-03-05 05:03:38 +0530

    middleware update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config();
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 
 //middlewarw
@@ -11,11 +13,11 @@ app.use(express.static('./public'))
 app.use(express.json())
 
 //routes
-// app.get('/hello',(req,res)=>{
-//     res.send("Task Maneger")
-// })
 
 app.use('/api/v1/tasks',tasks)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const port = 3000
 
