d8af6d59be53883588d8f202f97ecb1f7e4a37d2
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-03-05 01:51:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+
+//middlewarw
+
+app.use(express.json())
+
+//routes
+app.get('/hello',(req,res)=>{
+    res.send("Task Maneger")
+})
+
+app.use('/api/v1/tasks',tasks)
+
+const port = 3000
+
+app.listen(port,console.log(`Server is listening on port ${port}...`))
