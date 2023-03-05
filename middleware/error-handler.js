bd56c2bc11a9206bffb8d4c6ffef519ca3a08eeb
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-03-05 09:27:27 +0530

    .env init

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,9 @@
-const {CustomAPIError} = require('../errors/custom-error')
-const errorHandlerware = (err,req,res,next) =>{
-    if(err instanceof CustomAPIError){
-        return res.status(err.statusCode).json({msg:err.message})
-    }
-    return res.status(err.status).json({msg:err.message})
+const { CustomAPIError } = require('../errors/custom-error')
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message })
+  }
+  return res.status(500).json({ msg: 'Something went wrong, please try again' })
 }
 
-module.exports = errorHandlerware
+module.exports = errorHandlerMiddleware
